7ea8ea41ff8e6080f0c804651501219f86911760
rickrain <rickrain@hotmail.com>
2014-01-17 10:23:41 -0600

    Adding Blob Tests

---
+++ SCL3.Table.Tests/BlobTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace SCL3.Table.Tests
+{
+    [TestClass]
+    public class BlobTest : BaseTest
+    {
+        private CloudBlobContainer container = null;
+        private CloudBlockBlob blockBlob = null;
+        private CloudBlobClient blobClient = null;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
